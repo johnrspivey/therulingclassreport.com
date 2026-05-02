9f860a5534fee9e428a132738ba83d0d0d895bdf
johnrspivey <57794826+johnrspivey@users.noreply.github.com>
2026-05-02 05:48:54 -0500

    Update congress.js

--- netlify/functions/congress.js
+++ netlify/functions/congress.js
@@ -2,8 +2,8 @@ const CG_KEY = "5lsDCFzIBQ60fA6ZAc8JBzcWRcJBn6WW5L5m3I0H";
 const CG_BASE = "https://api.congress.gov/v3";
 
 exports.handler = async function(event) {
-  const path = event.queryStringParameters?.path || "";
   const params = { ...event.queryStringParameters };
+  const path = params.path || "";
   delete params.path;
   params.api_key = CG_KEY;
   params.format = "json";
@@ -11,13 +11,15 @@ exports.handler = async function(event) {
   const queryString = new URLSearchParams(params).toString();
   const url = `${CG_BASE}/${path}?${queryString}`;
 
+  console.log("Fetching:", url);
+
   try {
     const res = await fetch(url);
-    const data = await res.json();
+    const text = await res.text();
     return {
-      statusCode: 200,
-      headers: { "Access-Control-Allow-Origin": "*" },
-      body: JSON.stringify(data)
+      statusCode: res.status,
+      headers: { "Access-Control-Allow-Origin": "*", "Content-Type": "application/json" },
+      body: text
     };
   } catch (err) {
     return {
